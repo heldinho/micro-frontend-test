23f29429a9675a7e621b06f1333f922e6d6674d7
Helder Passos <helder.passos@mlins.com.br>
2021-07-21 16:46:30 -0300

    fix
    Signed-off-by: Helder Passos <helder.passos@mlins.com.br>

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <Todo pageTitle="Vue Tasks" />
-    <Tabs />
-    <button class="ant-btn ant-btn-primary">Teste de css atlas</button>
-    <a-button type="primary">Teste de css</a-button>
-    <button class="ant-btn">Teste</button>
+    <Content>
+      <Todo pageTitle="Vue Tasks" />
+      <Tabs />
+      <button class="ant-btn ant-btn-primary">Teste de css atlas</button>
+      <a-button type="primary">Teste de css</a-button>
+      <button class="ant-btn">Teste</button>
+    </Content>
   </div>
 </template>
 
 <script>
+import Content from './views/Client';
 import Todo from './views/Todo';
 import Tabs from './components/Tabs';
 export default {
   name: 'App',
-  components: { Todo, Tabs }
+  components: { Todo, Tabs, Content }
 };
 </script>
 
---
+++ vue-todo-list/src/views/Client.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-content class="a-layout-content">
+    <slot />
+  </a-layout-content>
+</template>
+
+<script>
+export default {
+  name: 'Client'
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
